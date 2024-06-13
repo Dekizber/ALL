31d00309b1d02a37b6c400ad7ba4bb4eddb1183c
dekizber <dekizber@gmail.com>
2024-06-13 22:02:49 +0300

    add

---
+++ JS/JS.js
@@ -0,0 +1,52 @@
+// function checkAge(age) {
+//     if (age >= 18) {
+//         return "You are an adult";
+//     }
+//     else { return 'little pisik' }
+
+// }
+
+
+// console.log(checkAge(15));
+
+// function checkStorage(available, ordered) {
+//     if (available <= ordered) {
+//         return "Not enough goods in stock!"
+//     }
+//     else { return "Order is processed, our manager will contact you" }
+
+// }
+
+// console.log(checkStorage(12, 14));
+
+
+// function checkStorage(available, ordered) {
+//     if (ordered === 0) { return "There are no products in the order!" }
+//     else if (available <= ordered) { return "Your order is too large, there are not enough items in stock!" }
+//     else { return "The order is accepted, our manager will contact you" }
+// }
+// console.log(checkStorage(15, 25));
+
+
+// function checkPassword(password) {
+//     const correctPassword = 'jqueryismyjam';
+//     return correctPassword === password ? 'Access granted' : 'Access denied, wrong password!';
+// }
+
+// console.log(checkPassword('jqueryismyjaewewem'));
+
+// function getSubscriptionPrice(type) {
+//     switch (type) {
+//         case "professional": return 20;
+//             break;
+//         case "organization": return 50;
+//             break;
+//         case "starter": return 0;
+//             break;
+
+//         default: return "Invalid subscription type!";
+
+//     }
+// }
+
+// console.log(getSubscriptionPrice("organization"));
